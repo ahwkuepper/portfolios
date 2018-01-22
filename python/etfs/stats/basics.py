4c51335f99ecbf78dc60f6a39f901600356c4741
Andreas Küpper <andreas.kupper@yale.edu>
2018-01-21 21:07:36 -0500

    added a first version of a volatility calculator

--- python/etfs/stats/basics.py
+++ python/etfs/stats/basics.py
@@ -1,14 +1,39 @@
-def runrate(df=None, column=None):
+def runrate(df=None, column=None, window=5, win_type=None):
    '''
+   Calculate the run rate, that is, the moving average,
+   of a column, and add it as a new column 
    '''
-   df = df
+   
+   column_rr = column + '_rr' + str(window)
+   df[column_rr] = df[column].rolling(window=window, win_type=win_type).mean()
 
    return df
 
 
-def resample(df=None, column=None, resolution='D'):
+def resample(df=None, column=None, resolution='B'):
    '''
+   Resample data with new resolution, return as new dataframe
    '''
-   df = df.resample(resolution).agg({column: ['mean', 'std', 'median']})
+   
+   return df.resample(resolution).agg({column: ['mean', 'std', 'median']})
+ 
+
+def volatility(df=None, column=None, window=5, shift=1):
+   '''
+   Calculate the standard deviation of data - run rate
+   '''
+   from pandas.tseries import offsets
+   
+   column_rr = column + '_rr' + str(window)
+   print("Calculating {0} run rate for window size {1}".format(column, window))
+   df = runrate(df, column=column, window=window)
+
+   column_shift = column_rr + '_sh' + str(shift)
+   df[column_shift] = df[column_rr].shift(shift, freq=offsets.BDay())
+
+   #column_volatility = column_shift + '_vol'
+   #df
 
    return df
+
+
